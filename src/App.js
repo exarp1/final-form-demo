751c0165f1ab09fef6fefec1aa35f0d30681a73f
exarp1 <exarp1@gmail.com>
2020-01-31 14:29:31 -0800

    adds three form fields and a submit button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { Form, Field } from 'react-final-form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div>
+        <h1>✓FINAL FORM✓</h1>
+        <Form
+          onSubmit={values =>
+            window.alert(JSON.stringify(values, undefined, 2))
+          }
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <div>
+                <TextField
+                  name={'firstName'}
+                  label={'First Name'}
+                  placeholder={'Enter First Name'}
+                />
+                <TextField
+                  name={'lastName'}
+                  label={'Last Name'}
+                  placeholder={'Enter Last Name'}
+                />
+                <TextField
+                  name={'email'}
+                  label={'email'}
+                  placeholder={'Enter email'}
+                  type={'email'}
+                />
+              </div>
+              <button type='submit'>Submit</button>
+            </form>
+          )}
+        </Form>
+      </div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export const TextField = props => {
+  const { error, touched, name, label, placeholder } = props
+  const styles = {
+    container: { margin: 10 },
+    error: {
+      marginTop: 5,
+      fontSize: '0.8em',
+      color: 'red',
+    },
+  }
+
+  return (
+    <Field>
+      {({ input, meta }) => (
+        <div style={styles.container}>
+          <label>{label}</label>
+          <Field component='input' {...props} />
+          {error && touched && <div style={styles.error}>{error}</div>}
+        </div>
+      )}
+    </Field>
+  )
+}
+
+export default App
